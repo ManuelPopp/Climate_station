c31aa857507e8b777ad63e0206d115b018155a2b
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-10-03 11:40:52 +0200

    Add files via upload

---
+++ Pico000/secrets0.py
@@ -0,0 +1,13 @@
+secrets = {
+    "ssid0": <SSID0>,
+    "password0": <PASSWORD0>,
+    "ssid1": <SSID1>,
+    "password1": <PASSWORD1>,
+    "timezone": "Europe/Berlin",
+    "mqtt_username": <CLIENT_NAME>,
+    "mqtt_key": <CLIENT_PASSWORD>,
+    "mqtt_client_id": "Station000",
+    "broker": None,
+    "port": 1883,
+    "topic": "stations/STATION000"
+    }
